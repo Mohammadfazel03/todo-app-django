37ad233e06262fac967c4c73e51b1ced25396b36
fazel <mohamadfazel1382@gmail.com>
2025-07-15 16:42:49 +0330

    feat: add jwt authentication

--- account/api/v1/urls.py
+++ account/api/v1/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework import routers
 from rest_framework.authtoken.views import ObtainAuthToken
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from account.api.v1.views import UserRegisterViewSet, LogoutApiView
 
 router = routers.SimpleRouter()
@@ -10,4 +10,6 @@ router.register(r'register', UserRegisterViewSet)
 urlpatterns = [
                   path('login', ObtainAuthToken.as_view()),
                   path('logout', LogoutApiView.as_view()),
+                  path('jwt/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+                  path('jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
               ] + router.urls
